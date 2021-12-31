e5b3e115279ffceeb71b293af32dcc009e85b3bc
Brian McClune <bpmcclune@gmail.com>
2021-12-31 11:28:32 -0500

    Update first function to handle question 4

--- assignment.R
+++ assignment.R
@@ -2,18 +2,8 @@
 library(ggplot2)
 
 #' Builds a plot to answer question 1
-plot_total_emissions <- function(nei, fips_code) {
-    if (missing(fips_code)) {
-        year_totals <- tapply(nei$Emissions, nei$year, sum)
-        main <- "Total Annual PM2.5 Emissions"
-    } else {
-        local <- subset(nei, nei$fips == fips_code)
-        year_totals <- tapply(local$Emissions, local$year, sum)
-        main <- sprintf(
-            "Total Annual PM2.5 Emissions, fips = %s",
-            fips_code
-        )
-    }
+plot_total_emissions <- function(nei, main) {
+    year_totals <- tapply(nei$Emissions, nei$year, sum)
     barplot(
         year_totals,
         xlab = "Year",
