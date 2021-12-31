5baf7e86e27dce498968f356ad0fe73cb41a66ee
Brian McClune <bpmcclune@gmail.com>
2021-12-31 11:39:40 -0500

    Refactor and add question5 function

--- assignment.R
+++ assignment.R
@@ -1,6 +1,8 @@
 #' A collection of functions to complete the course project.
 library(ggplot2)
 
+MAIN <- "Total Annual PM2.5 Emissions"
+
 #' Builds a plot to answer question 1
 plot_total_emissions <- function(nei, main) {
     year_totals <- tapply(nei$Emissions, nei$year, sum)
@@ -18,13 +20,12 @@ ggplot_emissions_by_type <- function(nei) {
 }
 
 question1 <- function(nei) {
-    main <- "Total Annual PM2.5 Emissions"
-    plot_total_emissions(nei, main)
+    plot_total_emissions(nei, MAIN)
 }
 
 question2 <- function(nei) {
     balt <- subset(nei, fips == "24510")
-    main <- "Total Annual PM2.5 Emissions for Baltimore City"
+    main <- paste(MAIN, "for Baltimore City")
     plot_total_emissions(balt, main)
 }
 
@@ -35,5 +36,14 @@ question3 <- function(nei) {
 question4 <- function(nei, scc) {
     coal_scc <- scc[grepl("^Fuel Comb.*Coal$", scc$EI.Sector), 1]
     coal_nei <- nei[nei$SCC %in% coal_scc]
-    ggplot_emissions_by_type(coal_nei)
+    main <- paste(MAIN, "from Coal Sources")
+    plot_total_emissions(coal_nei, main)
+}
+
+question5 <- function(nei, scc) {
+    mv_scc <- scc[grepl("^Mobile.*", scc$EI.Sector), 1]
+    mv_nei <- nei[nei$SCC %in% mv_scc, ]
+    mv_balt <- subset(mv_nei, fips == "24510")
+    main <- paste(MAIN, "from Mobile Sources in Baltimore City")
+    plot_total_emissions(mv_balt, main)
 }
