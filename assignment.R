7041331e0bf8db04fb0b7bae4bb1a2f25972ccaa
Brian McClune <bpmcclune@gmail.com>
2021-12-31 11:13:26 -0500

    Add function to answer question 3

--- assignment.R
+++ assignment.R
@@ -1,4 +1,5 @@
 #' A collection of functions to complete the course project.
+library(ggplot2)
 
 #' Builds a plot to answer question 1
 plot_total_emissions <- function(nei, fips_code) {
@@ -19,4 +20,9 @@ plot_total_emissions <- function(nei, fips_code) {
         ylab = "Total PM2.5 Emissions (tons)",
         main = main
     )
+}
+
+ggplot_emissions_by_type <- function(nei) {
+    g <- ggplot(nei, aes(factor(year), Emissions))
+    g + facet_grid(vars(type)) + geom_col()
 }
